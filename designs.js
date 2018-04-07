af697580dda4ede64e937d20809d896ebfe87b84
Tomas Kaiser <tomas.kaiser06@gmail.com>
2018-04-07 10:25:49 +0100

    Creating table

--- designs.js
+++ designs.js
@@ -14,4 +14,17 @@ const width = document.querySelector('#inputWeight').value;
 // Select table
 const table = document.querySelector('#pixelCanvas');
 
+// Delete table if there is
+table.innerHTML = '';
+
+// Create table
+for (let i = 1; i <= height; i++){
+    const newTr = document.createElement('tr');
+    table.appendChild(newTr);
+
+        for (let j = 1; j <= width; j++){
+            const newTd = document.createElement('td');
+            table.lastElementChild.appendChild(newTd)
+        }
+    }
 }
